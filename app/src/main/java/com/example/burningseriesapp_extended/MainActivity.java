d0abfeb352eb95030b62238435c987fbb7f94aa3
TeaNesx <nhatlong@hotmail.de>
2021-01-11 16:15:26 +0100

    getting html from bruning series. adding ion dependecie

--- app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
+++ app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
@@ -3,6 +3,18 @@ package com.example.burningseriesapp_extended;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import com.koushikdutta.async.future.FutureCallback;
+import com.koushikdutta.ion.Ion;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.net.URLConnection;
+import java.util.Scanner;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +22,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Ion.with(getApplicationContext()).load("https://burning-series.io/andere-serien").asString().setCallback(new FutureCallback<String>() {
+            @Override
+            public void onCompleted(Exception e, String result) {
+                System.out.println("result= " + result);
+            }
+        });
     }
 }
