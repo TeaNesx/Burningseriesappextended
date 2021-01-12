d795f93f4ca2cee3fc982a9f46a6add65cfddcc7
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2021-01-12 10:25:13 +0100

    Adding title to list view

--- app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
+++ app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
@@ -4,22 +4,29 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.ListAdapter;
+import android.widget.ListView;
 
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
 import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
 
     ArrayList<String> SerieList = new ArrayList<>();
+    private ListView lv_SerieLit;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv_SerieLit = findViewById(R.id.lv_Serie);
 
         (new Thread(new Runnable() {
             @Override
@@ -36,6 +43,8 @@ public class MainActivity extends AppCompatActivity {
                         }
 
                         System.out.println("SerieList: "+ SerieList);
+                        ArrayAdapter<String> adapter = new ArrayAdapter<String>(MainActivity.this, android.R.layout.simple_list_item_1, android.R.id.text1, SerieList);
+                        lv_SerieLit.setAdapter(adapter);
                     }
                 });
             }
