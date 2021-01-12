369095eba90364a8dd797a897205ea735a07acf2
TeaNesx <45711122+TeaNesx@users.noreply.github.com>
2021-01-12 09:08:11 +0100

    Getting Show name and adding into Arraylist.

--- app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
+++ app/src/main/java/com/example/burningseriesapp_extended/MainActivity.java
@@ -8,32 +8,38 @@ import android.util.Log;
 import com.koushikdutta.async.future.FutureCallback;
 import com.koushikdutta.ion.Ion;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.net.MalformedURLException;
-import java.net.URL;
-import java.net.URLConnection;
-import java.util.Scanner;
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    ArrayList<String> SerieList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Ion.with(getApplicationContext()).load("https://burning-series.io/andere-serien").asString().setCallback(new FutureCallback<String>() {
+        (new Thread(new Runnable() {
             @Override
-            public void onCompleted(Exception e, String result) {
-                Pattern pattern = Pattern.compile("<li><a href='serie/(.*?)'<a><li>", Pattern.DOTALL);
-                Matcher matcher = pattern.matcher(result);
-                while (matcher.find()) {
-                    System.out.println("matcher: "+matcher.group(1));
-                }
+            public void run() {
+                // Background executed code (Lambda-Expression)
+                Ion.with(getApplicationContext()).load("https://burning-series.io/andere-serien").asString().setCallback(new FutureCallback<String>() {
+                    @Override
+                    public void onCompleted(Exception e, String result) {
+                        Pattern pattern = Pattern.compile("<li><a href=\".+?\" title=\".+?\">(.+?)</a></li>", Pattern.DOTALL);
+                        Matcher matcher = pattern.matcher(result);
+                        while (matcher.find()) {
+                            System.out.println("matcher: "+matcher.group(1));
+                            SerieList.add(matcher.group(1));
+                        }
+
+                        System.out.println("SerieList: "+ SerieList);
+                    }
+                });
             }
-        });
+        })).start();
+
     }
 }
